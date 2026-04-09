36d50400ef9d3c6b3550c1a8c1b8a6b1cfbc60ec
Jaskeerat Singh <jaskeerat23006@gmail.com>
2026-04-10 00:02:04 +0530

    Implemented Authentication and Autherization

---
+++ server/routes/auth.middleware.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+async function authMiddleware(req, res, next) {
+    try {
+        const token = req.cookies.token;
+        if(!token) {
+            return res.status(400).json({
+                success: false,
+                message: "No Token Found"
+            });
+        }
+
+        const decode = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = decode;
+        next();
+    }
+    catch(err) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid token"
+        });
+    }
+}
+
+function authorizeRole(role) {
+    return (req, res, next) => {
+        const userRole = req.user.role;
+        if(userRole != role) {
+            return res.status(403).json({
+                success: false,
+                message: "User not authorized"
+            });
+        }
+        next();
+    }
+}
+
+module.exports = {
+    authMiddleware,
+    authorizeRole
+};
