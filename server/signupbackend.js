cdb6b9b46b8ae84d2259fe83834a61ddf3838583
Jaskeerat Singh <jaskeerat23006@gmail.com>
2026-04-10 00:08:16 +0530

    copy

--- server/signupbackend.js
+++ server/signupbackend.js
@@ -1,44 +1,86 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
+const user = require('./models/user.model.js');
+const rider = require('./models/rider.model.js');
+const organizer = require('./models/organizer.model.js');
+const sponsor = require('./models/sponsor.model.js');
+
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI_JAS)
-.then(() => {
-    console.log("Successfully connected to the database");
-})
-.catch((err) => {
-    console.error(err);
-})
+async function connect_to_db() {
+    await mongoose.connect(process.env.MONGODB_URI_JAS)
+    .then(() => {
+        console.log("Successfully connected to the database");
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+}
 
-const userSchema = new mongoose.Schema({
-    userName: {type: String, unique: true},
-    email: {type: String, unique: true},
-    password: {type: String},
-    role: {type: String},
-    createdAt: {type: String},
-    updatedAt: {type: String},
-});
+async function disconnect() {
+    await mongoose.disconnect();
+}
 
-const riderSchema = new mongoose.Schema({
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'Users', unique: true},
-    name: {type: String},
-    age: {type: Number},
-    city: {type: String},
-    socialLinks: {type: String},
-    bikes: {type: [String]},
-    ridingStyle: {type: [String]},
-    bio: {type: String}
-})
+async function insert_role_based(roleDetails, role, newObjId, session) {
+    try {
 
-const user = new mongoose.model('Users', userSchema, 'Users');
-const rider = new mongoose.model('Riders', riderSchema, 'Riders');
-// const org = new mongoose.model('Organizers', userSchema, 'Organizers');
-// const sponsor = new mongoose.model('Sponsors', userSchema, 'Sponsors');
+        if(role == 'rider') {
+
+            const name = roleDetails.name, age = roleDetails.age, city = roleDetails.city, socialLinks = roleDetails.socialLinks, bikes = roleDetails.bikes, ridingStyle = roleDetails.ridingStyle, bio = roleDetails.bio;
+
+            const newRider = new rider({
+                userId: newObjId,
+                name: name,
+                age: age,
+                city: city,
+                socialLinks: socialLinks,
+                bikes: bikes,
+                ridingStyle: ridingStyle,
+                bio: bio
+            });
+
+            await newRider.save({session})
+            console.log("rider successfully registered");
+        }
+        else if(role == 'organizer') {
+
+            const name = roleDetails.name, desc = roleDetails.description, socialLinks = roleDetails.socialLinks, website = roleDetails.website, gstin = roleDetails.gstin;
+
+            const newOrg = new organizer({
+                userId: newObjId,
+                name: name,
+                description: desc,
+                socialLinks: socialLinks,
+                website: website,
+                GSTIN: gstin
+            });
+
+            await newOrg.save({session});
+            console.log("organizer registered successfully");
+        }
+    }
+    catch(err) {
+        throw err;
+    }
+}
 
 async function sign_up_user(data) {
+
+    await connect_to_db();
+
+    console.log(data, "inside ");
     const username = data.username, email = data.email, pass = data.password, role = data.role;
     const roleDetails = data.roleDetails;
+
+    if (!['rider', 'organizer', 'sponsor'].includes(role)) {
+        throw new Error("Invalid role");
+    }
+    console.log("After role");
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
     const hash = await bcrypt.hash(pass, 10);
 
@@ -56,52 +98,50 @@ async function sign_up_user(data) {
 
     let newObjId = null;
     
-    await newUser.save()
-    .then(res => {
+    try {
+        const res = await newUser.save({session});
+        console.log("user registered");
+
         newObjId = res._id;
-    })
-    .catch(err => console.error(err));
 
-    if(role == 'rider') {
+        await insert_role_based(roleDetails, role, newObjId, session);
 
-        const name = roleDetails.name, age = roleDetails.age, city = roleDetails.city, socialLinks = roleDetails.socialLinks, bikes = roleDetails.bikes, ridingStyle = roleDetails.ridingStyle, bio = roleDetails.bio;
+        await session.commitTransaction();
+        session.endSession();
 
-        const newRider = new rider({
-            userId: newObjId,
-            name: name,
-            age: age,
-            city: city,
-            socialLinks: socialLinks,
-            bikes: bikes,
-            ridingStyle: ridingStyle,
-            bio: bio
-        });
+        return {
+            success: true,
+            data: {
+                _id: newObjId,
+                username: username,
+                email: email,
+                role: role
+            }   
+        };
+    }
+    catch(err) {
+        await session.abortTransaction();
+        console.error(err);
+        session.endSession();
 
-        await newRider.save()
-        .then(() => {
-            console.log("Successfully inserted rider");
-        })
-        .catch((err) => {
-            console.error(err);
-        })
+        throw err;
     }
 }
 
 data = {
-    username: 'jaskeerat23',
-    email: 'jas@mail.com',
+    username: 'adarsh2',
+    email: 'ad@@mai.c',
     password: 'abc123',
-    role: 'rider',
+    role: 'organizer',
     roleDetails: {
-        name: 'Jaskeerat Singh',
-        age: 20,
-        city: 'dehradun',
-        socailLinks: 'whatsapp',
-        bikes: ['KTM','ROYAL ENFILED'],
-        ridingStyle: ['Adventure', 'offroad'],
-        bio: 'i am a rider'
+        name: "re",
+        description: "i am the best",
+        socialLinks: "www.google.com",
+        website: "www.youtube.com",
+        gstin:  12212
     }
 }
 
-sign_up_user(data);
-await mongoose.disconnect();
+module.exports = {
+    sign_up_user
+};
