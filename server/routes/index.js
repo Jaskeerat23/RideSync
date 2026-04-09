7a4c3b71e56ba9f7b596936bad58563b44b9b69f
Jaskeerat Singh <jaskeerat23006@gmail.com>
2026-04-10 00:02:52 +0530

    used cors and updated code

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,16 +2,22 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const authRoutes = require('./auth.js');
-
+const dashboardRoutes = require('./dashboard.js');
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
 
 // Global middlewares
+app.use(cors({
+    origin: 'http://127.0.0.1:5500', // EXACT frontend origin
+    credentials: true                // allow cookies
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authRoutes);
+app.use('/dashboard', dashboardRoutes);
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
