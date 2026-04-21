5d215c87fcd792c0e9ee9a428600c7d932d9d28d
Jaskeerat Singh <jaskeerat23006@gmail.com>
2026-04-21 20:46:25 +0530

    server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-const authRoutes = require('./auth.js');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./auth.route.js');
 const dashboardRoutes = require('./dashboard.js');
 const cors = require('cors');
 dotenv.config();
 
 const app = express();
 
-// Global middlewares
+// Global middlewares 
 app.use(cors({
-    origin: 'http://127.0.0.1:5500', // EXACT frontend origin
+    origin: 'http://localhost:5500', // EXACT frontend origin
     credentials: true                // allow cookies
 }));
+
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
