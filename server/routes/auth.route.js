4d57f8dd049fc22443f2aa4962a5fac7ead0ce16
Jaskeerat Singh <jaskeerat23006@gmail.com>
2026-04-27 23:11:56 +0530

    used multer for parsing multipart data

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,13 +1,15 @@
 const authController = require('../controllers/auth.controller');
 console.log(authController);
 const express = require('express');
+const multer = require('multer');
 
+const upload = multer();
 const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({extended: true}));
 
 router.post('/login', authController.login);
-router.post('/signup', authController.signup);
+router.post('/signup', upload.single('pfp'), authController.signup);
 router.post('/google_sign_in', authController.google_sign_in);
 
 module.exports = router;
