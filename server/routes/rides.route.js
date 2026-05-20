cc12b93771aed526d88ed3cfd4ae387b3caa68a5
Jaskeerat Singh <jaskeerat23006@gmail.com>
2026-05-20 18:44:56 +0530

    Updated Routes (create_ride, ride_details)

--- server/routes/rides.route.js
+++ server/routes/rides.route.js
@@ -13,6 +13,6 @@ router.get('/rider_rides', authMiddleware.authMiddleware, authMiddleware.authori
 router.get('/ride_difficulty', authMiddleware.authMiddleware, authMiddleware.authorizeRole('rider'), rideController.get_rides_diff_based)
 router.get('/ride_type', authMiddleware.authMiddleware, authMiddleware.authorizeRole('rider'), rideController.get_rides_type_based)
 router.get('/org_rides', authMiddleware.authMiddleware, authMiddleware.authorizeRole('organizer'), rideController.get_org_rides);
-router.post('/create_ride', upload.single('banner'), authMiddleware.authMiddleware, authMiddleware.authorizeRole('organizer'), rideController.create_ride);
-router.get('/ride_details/:id', authMiddleware.authMiddleware, rideController.ride_details);
+router.post('/create_ride', upload.single('banner'), authMiddleware.authMiddleware, authMiddleware.authorizeRole('organizer'), rideController.create_ride); // ADARSH
+router.get('/ride_details/:id', authMiddleware.authMiddleware, rideController.ride_details); // YASH
 module.exports = router;
