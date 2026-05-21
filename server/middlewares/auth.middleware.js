a7d6f48f4375b6065168feadab6761cffa3f4306
Jaskeerat Singh <jaskeerat23006@gmail.com>
2026-05-21 12:45:13 +0530

    revert back to req.cookies.token only

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 async function authMiddleware(req, res, next) {
     try {
-        const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+        const token = req.cookies.token;
         if(!token) {
             return res.status(400).json({
                 success: false,
